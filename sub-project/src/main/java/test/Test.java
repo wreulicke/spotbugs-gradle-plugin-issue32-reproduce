c2aaaaa0f209afba29794c930af0a76fa42187f8
wreulicke <saito.masaya@classmethod.jp>
2018-06-10 07:07:24 +0900

    add source

---
+++ sub-project/src/main/java/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Test{
+
+    public static void main(String... args) {
+        // https://qiita.com/yuji38kwmt/items/adac8cfee43fdc3bf385
+        Integer a = 1000;
+        Integer b = 1000;
+        boolean rtn = a == b; //equalsメソッドを使うべき
+    }
+}
